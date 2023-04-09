0251573c59d7c92ed7ea1ee36c09fa15c26899c9
BioTeamCUIT <BioTeamCUIT@protonmail.com>
2023-04-09 13:43:01 +0800

    Data augmentation

--- Train_ten_five_cv.py
+++ Train_ten_five_cv.py
@@ -1,16 +1,21 @@
 import os
 import numpy as np
 from tqdm import tqdm
+import scipy.sparse as sp
+
 
 import torch
 import torch.nn.functional as F
 
-from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn import metrics
+from sklearn.model_selection import RepeatedStratifiedKFold
 
 from Utils.HGDataset import HGDataset
 
 from Models.HAN import HAN
+from Models.HGT import HGT
+
+from torch_geometric.utils import from_scipy_sparse_matrix
 
 
 def train(mask):
@@ -21,7 +26,8 @@ def train(mask):
         torch.sigmoid(out[data['gene'].label_index[mask]]).cpu().detach().numpy())
     # Attention: Use the mask to get the label index, then use the label index to get the genes used to train
     # loss = F.cross_entropy(out[data['gene'].label_index[mask]], data['gene'].y[mask])
-    loss = F.binary_cross_entropy_with_logits(input=out[data['gene'].label_index[mask]].squeeze(), target=data['gene'].y[mask])
+    loss = F.binary_cross_entropy_with_logits(input=out[data['gene'].label_index[mask]].squeeze(),
+                                              target=data['gene'].y[mask])
     acc = metrics.accuracy_score(y_true=data['gene'].y[mask].cpu(), y_pred=pred)
     loss.backward()
     optimizer.step()
@@ -34,7 +40,8 @@ def func_test(mask):
     out = model(data.x_dict, data.edge_index_dict)
     pred = np.round(
         torch.sigmoid(out[data['gene'].label_index[mask]]).cpu().detach().numpy())
-    loss = F.binary_cross_entropy_with_logits(input=out[data['gene'].label_index[mask]].squeeze(), target=data['gene'].y[mask])
+    loss = F.binary_cross_entropy_with_logits(input=out[data['gene'].label_index[mask]].squeeze(),
+                                              target=data['gene'].y[mask])
     acc = metrics.accuracy_score(y_true=data['gene'].y[mask].cpu(), y_pred=pred)
     auroc = metrics.roc_auc_score(data['gene'].y[mask].cpu(), pred)
     pr, rec, _ = metrics.precision_recall_curve(data['gene'].y[mask].cpu(), pred)
@@ -43,9 +50,59 @@ def func_test(mask):
     return pred, acc, auroc, aupr, loss.item()
 
 
+def drop_nodes(data, aug_ratio):
+    gene_node_num, _ = data['gene'].x.size()
+    _, gene_edge_num = data['gene', 'to', 'gene'].edge_index.size()
+    drop_num = int(gene_node_num * aug_ratio)
+
+    # Get the nodes to drop and keep
+    idx_perm = np.random.permutation(gene_node_num)
+    idx_drop = idx_perm[:drop_num]
+    idx_nondrop = idx_perm[drop_num:]
+    idx_nondrop.sort()
+
+    idx_dict = {idx_nondrop[n]: n for n in list(range(idx_nondrop.shape[0]))}
+    gene_edge_index = data['gene', 'to', 'gene'].edge_index.numpy()
+
+    # Get the new adj
+    gene_adj = torch.zeros((gene_node_num, gene_node_num))
+    gene_adj[gene_edge_index[0], gene_edge_index[1]] = 1
+    gene_adj = gene_adj[idx_nondrop, :][:, idx_nondrop]
+    # edge_index = from_scipy_sparse_matrix(sp.coo_matrix(gene_adj))[0]
+    edge_index = gene_adj.nonzero().t()
+
+    # # Remove the labels if the nodes were dropped
+    # label_index = data['gene'].label_index[~np.isin(data['gene'].label_index, torch.tensor(idx_drop))]
+    # # Re-index the label index
+    # label_index_np = label_index.numpy()
+    # for index, item in enumerate(label_index_np):
+    #     if item in idx_dict:
+    #         label_index_np[index] = idx_dict[item]
+    # label_index = torch.tensor(label_index_np)
+    # data['gene'].label_index = label_index
+    labels = data['gene'].y.numpy()
+    label_index_np = data['gene'].label_index.numpy()
+    new_labels = []
+    new_label_index = []
+    for index, item in enumerate(label_index_np):
+        if item in idx_dict:
+            new_labels.append(labels[index])
+            new_label_index.append(idx_dict[item])
+    try:
+        data['gene'].x = data['gene'].x[idx_nondrop]
+        data['gene'].y = torch.tensor(new_labels)
+        data['gene'].label_index = torch.tensor(new_label_index)
+        data['gene', 'to', 'gene'].edge_index = edge_index
+    except:
+        data = data
+    return data
+
+
 if __name__ == '__main__':
     dataset = HGDataset(root="E:\\xiong\\Bioinfomatics\\DriverGenes\\WorkingSpace\\MyDriveGenes")
     data = dataset[0]
+    data1 = drop_nodes(data, 0.2)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     folds = 2
@@ -61,6 +118,7 @@ if __name__ == '__main__':
     pbar = tqdm(enumerate(splits))
     for i, (train_mask, val_mask) in pbar:
         model = HAN(metadata=data.metadata(), in_channels=-1, out_channels=1)
+        # model = HGT(hidden_channels=64, out_channels=1, num_heads=2, num_layers=1, data=data)
         data, model = data.to(device), model.to(device)
 
         with torch.no_grad():  # Initialize lazy modules.
@@ -82,4 +140,3 @@ if __name__ == '__main__':
         # Train_ACC[i, (i % 5) + 1] = train_acc
 
         # break
-
